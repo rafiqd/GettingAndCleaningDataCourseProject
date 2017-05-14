40d9afd262def13d164c09cdd46728331622c626
rafiqd <rafiq.dandoo@gmail.com>
2017-05-13 22:04:20 -0700

    added descriptive var names

--- run_analysis.R
+++ run_analysis.R
@@ -12,7 +12,9 @@ get_data_from_file <- function(path, col_names){
     dat <- read.table(file.path(path, dat_file), col.names = col_names)
     train_activity <- read.table(file.path(path, act_file), col.names = c("Activity"))
     subject <- read.table(file.path(path, sub_file), col.names = c("Subject"))
+
     full_table <- cbind(dat, train_activity, subject)
+                        
     return(full_table)
 }
 
@@ -23,13 +25,14 @@ create_aggregate_data <- function(){
     title_vector <- get_col_titles(title_file)
     test_dat <- get_data_from_file('./UCI HAR Dataset/test', title_vector)
     train_dat <- get_data_from_file('./UCI HAR Dataset/train', title_vector)
-    dat <- rbind(train_dat, test_dat)
-    
     activity_factors_path = './UCI HAR Dataset/activity_labels.txt'
     labels <- read.table(activity_factors_path)
-    factor_data <- factor(labels[,1], labels=labels[,2])
-    
+    dat <- rbind(train_dat, test_dat)
     new_dat <- dat[, grep(c("[Mm][Ee][Aa][Nn]|[Ss][Tt][Dd]|Activity|Subject"), names(dat))]
+    
     aggregate_dat <- aggregate(new_dat, by=c(new_dat["Activity"], new_dat["Subject"]), FUN = mean)
-    return(aggregate_dat)
+    Activity <- factor(aggregate_dat[,"Activity"], labels[,1], labels=labels[,2])
+    full_data <- cbind(aggregate_dat[, !(colnames(aggregate_dat) %in% c("Activity"))], Activity)
+    write.table(full_data, 'data.txt', row.names=FALSE)
+    return(full_data)
 }
